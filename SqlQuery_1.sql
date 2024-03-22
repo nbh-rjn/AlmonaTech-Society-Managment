b5f26745152efae1eef94727e2f20f7533e18c39
Moin Haider <moinhaider256@gmail.com>
2024-03-22 13:51:24 +0400

    The db and the queries have been added.

---
+++ SqlQuery_1.sql
@@ -0,0 +1,53 @@
+﻿create database societydb
+use societydb
+
+create table User_(
+	userid int identity(1,1),
+	primary key(userid),
+	fname varchar(50),
+	lname varchar(50),
+	email varchar(50),
+	usertype varchar(10),
+	pass varchar(20)
+)
+
+create table eventRequest
+(
+	reqID int identity(1,1),
+	primary key(reqID),
+	eventName varchar(100),
+	description_ varchar(250),
+	date_ date,
+	userID int
+	foreign key (userID) references User_(userID)
+)
+
+create table Society
+(
+	societyID int identity(1,1),
+	primary key(societyID),
+	sname varchar(100),
+	description varchar(max)
+)
+
+create table events_
+(
+    eventID int identity(1,1),
+	primary key(eventID),
+	eventName varchar(100),
+	societyID int,
+	eventDateTime datetime,
+	eventLocation varchar(100),
+	eventDescription varchar(max)
+)
+
+create table announcements
+(
+    userID int,
+	announcementID int identity(1,1),
+	announcementTitle varchar(100),
+	announcementDesc varchar(max),
+	announcementDateTime datetime,
+	foreign key (userID) references User_(userID)
+
+)
---
+++ SqlQuery_2.sql
@@ -0,0 +1,20 @@
+﻿use SE_DB
+
+-- userstory 01 query
+select * from Society
+
+-- userstory 02 query (signup)
+-- to see if the email already exists in the db
+ select count(*) from User_ where email = @email;
+
+-- to insert the details into the db
+insert into User_ (fname, lname, email, usertype, pass)
+values (@fname, @lname, @email, @usertype, @pass);
+
+-- userstory 03 query
+insert into events_ (eventName, societyID, eventDateTime, eventLocation, eventDescription)
+values (@eventName, @societyID, @eventDateTime, @eventLocation, @eventDescription);
+
+-- userstory 04 query
+insert into Society (sname, description)
+values (@sname, @description);
