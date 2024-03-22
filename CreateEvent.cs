82244dc7e00e1d8e62e79cd0b6f33f4ad97b997b
rajan <rajan@HOME>
2024-03-22 17:07:31 +0500

    create event page added

---
+++ CreateEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlmonaTech_Society_Managment
+{
+    public partial class CreateEvent : Form
+    {
+        public CreateEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
