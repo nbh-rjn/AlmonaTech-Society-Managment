69e90abe2c004d208a3c2e64a62731239913c97e
nbh-rjn <156829737+nbh-rjn@users.noreply.github.com>
2024-04-28 18:51:35 +0500

    cleaning up database

---
+++ final db updates.sql
@@ -0,0 +1,14 @@
+﻿ALTER TABLE memberRequest
+ALTER COLUMN societyID INT NOT NULL;
+
+ALTER TABLE memberRequest
+ALTER COLUMN userID INT NOT NULL;
+
+ALTER TABLE memberRequest
+ADD CONSTRAINT PK_memberRequest PRIMARY KEY (userID, societyID);
+
+ALTER TABLE announcements
+DROP COLUMN announcementTitle;
+
+ALTER TABLE announcements
+DROP COLUMN announcementDateTime;
